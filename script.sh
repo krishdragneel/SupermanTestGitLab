17c7de2f37725133c045c27bb531d89ed52dd35a
krishdragneel <35262371+krishdragneel@users.noreply.github.com>
2023-07-02 01:12:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install nginx -y
-echo "Hi Superman" >/var/www/html/index.nginx-debian.html
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo echo "Hi Superman" >/var/www/html/index.nginx-debian.html
 
 
 
-#       "sudo apt-get update",
-#       "sudo apt-get install apache2 -y",
